93cfb5d05cd9ddccb5524459f818ed5f2409d560
Izza Nadeem <izza.nadeem99@gmail.com>
2017-12-08 15:47:13 -0500

    what i did so far

---
+++ AC-iOS-MidProgramAssessment/ViewControllers/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  AC-iOS-MidProgramAssessment
+//
+//  Created by C4Q on 12/8/17.
+//  Copyright © 2017 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var elementImage: UIImageView!
+    
+    @IBOutlet weak var symbolNumberWeight: UILabel!
+    @IBOutlet weak var name: UILabel!
+    
+    
+}
---
+++ AC-iOS-MidProgramAssessment/ViewControllers/NewFavoriteViewController.swift
@@ -0,0 +1,23 @@
+//
+//  NewFavoriteViewController.swift
+//  AC-iOS-MidProgramAssessment
+//
+//  Created by C4Q on 12/8/17.
+//  Copyright © 2017 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class NewFavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+
+
+
+}
