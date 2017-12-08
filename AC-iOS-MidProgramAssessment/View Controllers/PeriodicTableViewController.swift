75ee52d7c390af122154a3f7bbc5525bbc3151be
Melissa He <melissahe@ac.c4q.nyc>
2017-12-08 10:54:20 -0500

    Set up auto layout for table view, set up table view cell, prepare for setting up table view controller

---
+++ AC-iOS-MidProgramAssessment/View Controllers/PeriodicTableViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  AC-iOS-MidProgramAssessment
+//
+//  Created by C4Q  on 12/7/17.
+//  Copyright © 2017 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class PeriodicTableViewController: UIViewController {
+    
+    @IBOutlet weak var periodicTableView: UITableView!
+    
+    var elements: [Element] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        periodicTableView.delegate = self
+        periodicTableView.dataSource = self
+        loadData()
+    }
+    
+    func loadData() {
+        
+    }
+    
+    //Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //to do
+    }
+
+}
+
+//Table View Methods
+extension PeriodicTableViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    //Delegate Methods
+    func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        // to do
+    }
+    
+    //Data Source Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return elements.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "elementCell", for: indexPath)
+        
+        if let elementCell = cell as? ElementTableViewCell {
+            
+            //to do
+            
+            return elementCell
+        }
+        
+        return cell
+    }
+}
