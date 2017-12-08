835f787d67bfae1a0d2b0409704ea6e32dddc861
Melissa He <melissahe@ac.c4q.nyc>
2017-12-08 12:42:07 -0500

    Add network request manager, add error enum for error handling

--- AC-iOS-MidProgramAssessment/AppError.swift
+++ AC-iOS-MidProgramAssessment/AppError.swift
@@ -9,5 +9,11 @@
 import Foundation
 
 enum AppError: Error {
-    
+    case badData
+    case badImageURL
+    case badStatusCode(num: Int)
+    case badURL
+    case couldNotParseJSON(rawError: Error)
+    case noInternet
+    case other(rawError: Error)
 }
--- AC-iOS-MidProgramAssessment/NetworkHelper.swift
+++ AC-iOS-MidProgramAssessment/NetworkHelper.swift
@@ -16,9 +16,41 @@ class NetworkHelper {
         urlSession.dataTask(
         with: urlRequest) { (data, response, error) in
             DispatchQueue.main.async {
-             
+                if let error = error {
+                    errorHandler(AppError.other(rawError: error))
+                    return
+                }
                 
+                if let response = response as? HTTPURLResponse, response.statusCode < 200, response.statusCode > 300 {
+                    errorHandler(AppError.badStatusCode(num: response.statusCode))
+                    return
+                }
+                
+                if let data = data {
+                    completionHandler(data)
+                }
             }
         }.resume()
+    }
+    
+    func getData(from url: URL, completionHandler: @escaping (Data) -> Void, errorHandler: @escaping (Error) -> Void) {
+        urlSession.dataTask(
+        with: url) { (data, response, error) in
+            DispatchQueue.main.async {
+                if let error = error {
+                    errorHandler(AppError.other(rawError: error))
+                    return
+                }
+                
+                if let response = response as? HTTPURLResponse, response.statusCode < 200, response.statusCode > 300 {
+                    errorHandler(AppError.badStatusCode(num: response.statusCode))
+                    return
+                }
+                
+                if let data = data {
+                    completionHandler(data)
+                }
+            }
+            }.resume()
     }
 }
