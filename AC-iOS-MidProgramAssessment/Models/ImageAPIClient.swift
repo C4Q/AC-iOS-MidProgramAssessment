0789520fdb9847e35fe4352f52f9152fd0ea2c90
Marlon Rugama <MarlonRugama@ac.c4q.nyc>
2017-12-08 16:06:28 -0500

    MRUGAMA

---
+++ AC-iOS-MidProgramAssessment/Models/ImageAPIClient.swift
@@ -0,0 +1,32 @@
+//
+//  ImageAPIClient.swift
+//  AC-iOS-MidProgramAssessment
+//
+//  Created by C4Q on 12/8/17.
+//  Copyright © 2017 C4Q . All rights reserved.
+//
+
+import Foundation
+import UIKit
+class ImageAPIClient {
+    private init() {}
+    static let manager = ImageAPIClient()
+    func getImage(from urlStr: String,
+                  completionHandler: @escaping (UIImage) -> Void,
+                  errorHandler: @escaping (Error) -> Void) {
+        guard let url = URL(string: urlStr) else {
+            errorHandler(AppError.badURL)
+            return }
+        let request = URLRequest(url: url)
+        let completion: (Data) -> Void = {(data: Data) in
+            guard let onlineImage = UIImage(data: data) else {
+                errorHandler(AppError.notAnImage)
+                return
+            }
+            completionHandler(onlineImage)
+        }
+        NetworkHelper.manager.performDataTask(with: request,
+                                              completionHandler: completion,
+                                              errorHandler: errorHandler)
+    }
+}
---
+++ AC-iOS-MidProgramAssessment/View Controllers/ElementTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ElementTableViewCell.swift
+//  AC-iOS-MidProgramAssessment
+//
+//  Created by C4Q on 12/8/17.
+//  Copyright © 2017 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class ElementTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var symbolAtomicLabel: UILabel!
+    @IBOutlet weak var elementImageView: UIImageView!
+    
+}
