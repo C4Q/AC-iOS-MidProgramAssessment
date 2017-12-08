dae8bc65183f3e9d450df53fad62c4c421ce2db7
Luis Calle <lcalle101qc@gmail.com>
2017-12-08 12:04:24 -0500

    detailed view working

--- AC-iOS-MidProgramAssessment/View Controllers/ElementDetailViewController.swift
+++ AC-iOS-MidProgramAssessment/View Controllers/ElementDetailViewController.swift
@@ -10,12 +10,36 @@ import UIKit
 
 class ElementDetailViewController: UIViewController {
     
+    var element: Element?
     
-
+    @IBOutlet weak var titleNameLabel: UILabel!
+    @IBOutlet weak var elementImage: UIImageView!
+    @IBOutlet weak var meltingPointLabel: UILabel!
+    @IBOutlet weak var boilingPointLabel: UILabel!
+    @IBOutlet weak var discoveryNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let element = element else { return }
+        titleNameLabel.text = element.name
+        meltingPointLabel.text = element.melting_c?.description ?? "Melting point N/A"
+        boilingPointLabel.text = element.boiling_c?.description ?? "Boiling point N/A"
+        discoveryNameLabel.text = "Coming soon..."
+        loadLargeElemImage(element: element)
+    }
+    
+    func loadLargeElemImage(element: Element) {
+        let largeImageURLStr = "http://images-of-elements.com/\(element.name.lowercased()).jpg"
+        let imageCompletion: (UIImage) -> Void = { (onlineLargeImage: UIImage) in
+            self.elementImage.image = onlineLargeImage
+            self.elementImage.setNeedsLayout()
+        }
+        ImageAPIClient.manager.getImage(from: largeImageURLStr, completionHandler: imageCompletion, errorHandler: {print($0)} )
     }
 
+    @IBAction func favoriteButtonPressed(_ sender: UIButton) {
+        
+    }
     
 
 }
--- AC-iOS-MidProgramAssessment/View Controllers/ElementsViewController.swift
+++ AC-iOS-MidProgramAssessment/View Controllers/ElementsViewController.swift
@@ -32,15 +32,14 @@ class ElementsViewController: UIViewController {
         }
         ElementAPIClient.manager.getElements(from: urlStr, completionHandler: completion, errorHandler: { print($0) })
     }
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if let destination = segue.destination as? ElementDetailViewController {
+            guard let indexPathselected = elementsTableView.indexPathForSelectedRow else { return }
+            let selectedElement = self.elements[indexPathselected.row]
+            destination.element = selectedElement
+        }
     }
-    */
 
 }
 
