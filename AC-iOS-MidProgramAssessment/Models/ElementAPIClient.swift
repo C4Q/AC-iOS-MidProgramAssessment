081f3f833d1e5c6cf0c6238f150990611770f46c
Ashlee Krammer <ashlee.alyse@me.com>
2017-12-08 10:51:50 -0500

    Added VC

---
+++ AC-iOS-MidProgramAssessment/Models/ElementAPIClient.swift
@@ -0,0 +1,9 @@
+//
+//  ElementAPIClient.swift
+//  AC-iOS-MidProgramAssessment
+//
+//  Created by Ashlee Krammer on 12/8/17.
+//  Copyright © 2017 C4Q . All rights reserved.
+//
+
+import Foundation
