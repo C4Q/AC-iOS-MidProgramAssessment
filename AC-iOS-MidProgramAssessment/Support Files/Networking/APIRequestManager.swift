cf07fe54d5e49c403b71c37a6a9d50aec25dda40
Marty Hernandez Avedon <martyavedon@gmail.com>
2018-02-15 02:09:03 -0500

    working on api request manager & basic auth constants

--- AC-iOS-MidProgramAssessment/Support Files/Networking/APIRequestManager.swift
+++ AC-iOS-MidProgramAssessment/Support Files/Networking/APIRequestManager.swift
@@ -12,4 +12,68 @@ class APIRequestManager {
     private init() {}
     
     static let shared = APIRequestManager()
+    
+    func getData(from endPoint: String, callback: @escaping (Data?) -> Void) {
+        guard let myURL = URL(string: endPoint) else { return }
+        
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        session.dataTask(with: myURL) { (data: Data?, response: URLResponse?, error: Error?) in
+            if let errorFound = error {
+                print("Error durring session: \(errorFound.localizedDescription)")
+            }
+            
+            guard let validData = data else { return }
+            
+            callback(validData)
+            }.resume()
+    }
+    
+    func postRequest(to endPoint: String, data: [String : Any]) {
+        guard let url = URL(string: endPoint) else { return }
+        
+        var request = URLRequest(url: url)        
+        let namePassStr = "\(BasicAuth.username):\(BasicAuth.password)"
+        let nameAndPassData = namePassStr.data(using: .utf8)!
+        let base64AuthEncoding = nameAndPassData.base64EncodedString()
+        let authStr = "Basic \(base64AuthEncoding)"
+        
+        request.httpMethod = "POST"
+        request.addValue(authStr, forHTTPHeaderField: "Authorization")
+       // request.addValue("application/json", forHTTPHeaderField: "Accept")
+       // request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        do {
+            let body = try JSONSerialization.data(withJSONObject: data, options: [])
+            request.httpBody = body
+        } 
+            
+        catch {
+            print("Error posting body: \(error.localizedDescription)")
+        }
+        
+        let session = URLSession(configuration: .default)
+        session.dataTask(with: request) { (data, response, error) in
+            if let foundError = error {
+                print("Error encountered during post request: \(foundError.localizedDescription)")
+            }
+            
+            if let gotResponse = response {
+                let httpResponse = gotResponse as! HTTPURLResponse
+                
+                print("Response status code: \(httpResponse.statusCode)")
+            }
+            
+            guard let validData = data else { return }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: validData, options: []) as? [String:Any]
+                
+                if let validJson = json { print(validJson) }
+            }
+                
+            catch {
+                print("Error converting json: \(error)")
+            }
+            }.resume()
+    }
 }
