798242676acebb21a824697d147091f22661b632
Marty Hernandez Avedon <martyavedon@gmail.com>
2018-02-15 01:37:49 -0500

    created element inits

--- AC-iOS-MidProgramAssessment/Models/Element.swift
+++ AC-iOS-MidProgramAssessment/Models/Element.swift
@@ -8,4 +8,35 @@
 
 import Foundation
 
-class Element {}
+class Element {    
+    let name: String
+    let symbol: String
+    let number: Int
+    let weight: Double
+    let meltingPoint: Int?
+    let boilingPoint: Int?
+    // this could be a number or text, so let's make it easier on ourselves by storing any numbers we get as Strings 
+    let yearDiscovered: String 
+    let thumbnailURL: URL?
+    let portraitURL: URL?
+    
+    init(name: String, symbol: String, number: Int, weight: Double, meltingPoint: Int?, boilingPoint: Int?, yearDiscovered: String, thumbnailURL: URL?, portraitURL: URL?) {
+        self.name = name
+        self.symbol = symbol
+        self.number = number
+        self.weight = weight
+        self.meltingPoint = meltingPoint
+        self.boilingPoint = boilingPoint
+        self.yearDiscovered = yearDiscovered
+        self.thumbnailURL = thumbnailURL
+        self.portraitURL = portraitURL
+    }
+    
+    convenience init(name: String, symbol: String, number: Int, weight: Double, meltingPoint: Int?, boilingPoint: Int?, yearDiscovered: String, thumbnailAddress: String, portraitAddress: String) {
+       
+        let thumbnailURL = URL(string: thumbnailAddress) ?? nil
+        let portraitURL = URL(string: portraitAddress) ?? nil
+        
+        self.init(name: name, symbol: symbol, number: number, weight: weight, meltingPoint: meltingPoint, boilingPoint: boilingPoint, yearDiscovered: yearDiscovered, thumbnailURL: thumbnailURL, portraitURL: portraitURL)
+    }
+}
